45292549afead7b83874976f826748ffd3110aeb
Mseunghwan <minccc42710@gmail.com>
2025-12-09 04:21:06 +0900

    컴포넌트 스캔과 의존관계 설정 https://www.youtube.com/watch?v=7sw_GpkVwfM&list=PLumVmq_uRGHgBrimIp2-7MCnoPUskVMnd&index=14

--- src/main/java/org/example/kimyeonghanbasic/repository/MemoryMemberRepository.java
+++ src/main/java/org/example/kimyeonghanbasic/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package org.example.kimyeonghanbasic.repository;
 
 import org.example.kimyeonghanbasic.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -10,6 +11,7 @@ import java.util.*;
  * --> 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
--- src/main/java/org/example/kimyeonghanbasic/service/MemberService.java
+++ src/main/java/org/example/kimyeonghanbasic/service/MemberService.java
@@ -3,10 +3,12 @@ package org.example.kimyeonghanbasic.service;
 import org.example.kimyeonghanbasic.domain.Member;
 import org.example.kimyeonghanbasic.repository.MemberRepository;
 import org.example.kimyeonghanbasic.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
