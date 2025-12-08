9d0323e4cbf00d546c39cc9576c98b71126c5749
Mseunghwan <minccc42710@gmail.com>
2025-12-09 03:49:23 +0900

    회원 도메인과 레포지토리 만들기 https://www.youtube.com/watch?v=Gul8sv7cf8g&list=PLumVmq_uRGHgBrimIp2-7MCnoPUskVMnd&index=10

--- src/main/java/org/example/kimyeonghanbasic/controller/HelloController.java
+++ src/main/java/org/example/kimyeonghanbasic/controller/HelloController.java
@@ -20,5 +20,4 @@ public class HelloController {
         return "hello-template";
     }
 
-
 }
---
+++ src/main/java/org/example/kimyeonghanbasic/domain/Member.java
@@ -0,0 +1,24 @@
+package org.example.kimyeonghanbasic.domain;
+
+public class Member {
+
+    private Long id;
+
+    private String name;
+
+    private Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
