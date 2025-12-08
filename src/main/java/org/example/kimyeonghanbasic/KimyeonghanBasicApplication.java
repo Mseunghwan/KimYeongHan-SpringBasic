061088a35be0825f2a2862fd74c84ba0980e9795
Mseunghwan <minccc42710@gmail.com>
2025-12-09 03:19:49 +0900

    feat : Initial Commit

---
+++ src/main/java/org/example/kimyeonghanbasic/KimyeonghanBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.kimyeonghanbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyeonghanBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimyeonghanBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kimyeonghanbasic/KimyeonghanBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kimyeonghanbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimyeonghanBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
