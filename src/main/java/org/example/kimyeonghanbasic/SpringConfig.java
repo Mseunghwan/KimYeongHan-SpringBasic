24d18a0c10c6cc3d966d34adf72c14639605108d
Mseunghwan <minccc42710@gmail.com>
2025-12-10 02:47:54 +0900

    jpa https://www.youtube.com/watch?v=H9VBDCAtRG8&list=PLumVmq_uRGHgBrimIp2-7MCnoPUskVMnd&index=23

--- src/main/java/org/example/kimyeonghanbasic/SpringConfig.java
+++ src/main/java/org/example/kimyeonghanbasic/SpringConfig.java
@@ -1,6 +1,8 @@
 package org.example.kimyeonghanbasic;
 
+import jakarta.persistence.EntityManager;
 import org.example.kimyeonghanbasic.repository.JdbcMemberRepository;
+import org.example.kimyeonghanbasic.repository.JpaMemberRepository;
 import org.example.kimyeonghanbasic.repository.MemberRepository;
 import org.example.kimyeonghanbasic.repository.MemoryMemberRepository;
 import org.example.kimyeonghanbasic.service.MemberService;
@@ -13,13 +15,12 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
-
 
     @Bean
     public MemberService memberService() {
@@ -30,7 +31,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
         // 생성자 주입의 장점, JdbcMemberRepository 구현체 만들어서, Config 파일만 고쳐도 O
         // 다형성을 활용한다, 즉 인터페이스를 두고 구현체를 갈아낀다
         // 스프링 컨테이너가 DI를 도와주는 덕분
