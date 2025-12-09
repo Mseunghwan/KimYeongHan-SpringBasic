e119002ec0f3d2a4d4a9947923d74275b50b6759
Mseunghwan <minccc42710@gmail.com>
2025-12-10 00:20:50 +0900

    회원 웹 기능 - 조회 https://www.youtube.com/watch?v=wCCMoUatat8&list=PLumVmq_uRGHgBrimIp2-7MCnoPUskVMnd&index=18

--- src/main/java/org/example/kimyeonghanbasic/controller/MemberController.java
+++ src/main/java/org/example/kimyeonghanbasic/controller/MemberController.java
@@ -4,8 +4,11 @@ import org.example.kimyeonghanbasic.domain.Member;
 import org.example.kimyeonghanbasic.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,4 +35,10 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
